06504ac124959b55d7d2a20b6090c3f8bc1ff584
pjulmeus <pjulmeus@gmail.com>
2024-09-21 17:18:53 -0400

    finished the app first draft now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
 
   return ( 
-    <div className="App">
+    <div className="App container">
       <NavBar/>
         <Routes>
             <Route path="/" element={<Home />} />
--- my-app/src/CertificationLabels.js
+++ my-app/src/CertificationLabels.js
@@ -8,7 +8,7 @@ return (
     <Stack className="stack_space_labels" gap={1} direction="horizontal">
         <label className="left_side"><b>Where</b></label>
         <label className="middle_side" > <b>Type of Certification</b></label>
-        <label className="right_side"> <b>The link </b></label>
+        <label className="right_side"> <b>Link</b></label>
   </Stack>
 )
 }
